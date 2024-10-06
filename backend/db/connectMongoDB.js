6e4077040c5e0921625b1adf428ae5ffd1600553
Kürşat Polatcı <polatcikursat58@gmail.com>
2024-10-06 12:57:02 +0300

    Project Setup

---
+++ backend/db/connectMongoDB.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectMongoDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB connected: ${conn.connection.host}`);
+        console.log(`MONGO_URI: ${process.env.MONGO_URI}`)
+    } catch (error) {
+        console.log(`Error connection to MongoDB: ${error.message}`)
+        process.exit(1);
+    }
+}
+
+export default connectMongoDB;
---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv"
+import { v2 as cloudinary } from "cloudinary";
+import cookieParser from "cookie-parser";
+
+import connectMongoDB from "./db/connectMongoDB.js";
+
+dotenv.config();
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+app.use(cookieParser());
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT} port`)
+    connectMongoDB();
+})

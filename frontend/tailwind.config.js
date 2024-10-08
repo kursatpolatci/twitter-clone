b49d473d326622afeed31253aa81b70434448463
Kürşat Polatcı <polatcikursat58@gmail.com>
2024-10-08 13:08:21 +0300

    UI Design Completed

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,27 @@
+import daisyui from "daisyui";
+import daisyUIThemes from "daisyui/src/theming/themes";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
+	content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}"
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	theme: {
+		extend: {},
+	},
+	plugins: [daisyui],
 
+	daisyui: {
+		themes: [
+			"light",
+			{
+				black: {
+					...daisyUIThemes["black"],
+					primary: "rgb(29, 155, 240)",
+					secondary: "rgb(24, 24, 24)",
+				},
+			},
+		],
+	},
+};

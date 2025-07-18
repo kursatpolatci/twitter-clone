30fe3c8d3e326665b432a0f9370cd862730881e0
Kürşat Polatcı <polatcikursat58@gmail.com>
2025-07-18 18:57:33 +0300

    Updated

---
+++ backend/app.js
@@ -0,0 +1,34 @@
+import dotnev from 'dotenv';
+import { v2 as cloudinary } from 'cloudinary';
+import express from 'express';
+import cookieParser from 'cookie-parser';
+
+import authRoutes from './routes/auth.route.js';
+import userRoutes from './routes/user.route.js';
+import postRoutes from './routes/post.route.js';
+import notificationRoutes from './routes/notification.route.js';
+
+dotnev.config({});
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const app = express();
+
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/posts', postRoutes);
+app.use('/api/notifications', notificationRoutes);
+
+export default app;
